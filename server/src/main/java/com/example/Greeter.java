e1fbdce96990f36c2c2dcdb84867195a2054f0a5
Tunde <tunde1990@gmail.com>
2020-09-11 07:43:50 +0200

    feat(): wip

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,11 +12,7 @@ public class Greeter {
 
   }
 
-  /**
-   * Greet someone.
-   * @param  someone The name of the person
-   * @return         Greeting
-   */
+  /**  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
